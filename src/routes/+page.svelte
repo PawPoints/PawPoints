8c2abd65b375bdecf3cf968175970dc14deed0bb
Barış Bayazıt <bayazitbaris@gmail.com>
2023-11-04 15:23:49 -0400

    add tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
-    import '../app.css'
+	import '../app.postcss'
 </script>
 
 <div>
-    <div>
-        <!-- <img class="object-fit" src="https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*" alt=""> -->
-        <div class="text-red-900">asddsaf</div>
-        <div class="description"></div>
-    </div>
+	<div>
+		<div class="text-red-900 font-bold">asddsaf</div>
+		<div class="description" />
+	</div>
 </div>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: white;
+	}
+</style>
