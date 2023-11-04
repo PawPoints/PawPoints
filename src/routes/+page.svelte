4f95a7df04a8be618d0454175657e949a74ef931
Zev-G <godfreyzev@gmail.com>
2023-11-04 15:04:03 -0400

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import '../app.css'
+</script>
+
+<div>
+    <div>
+        <!-- <img class="object-fit" src="https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*" alt=""> -->
+        <div class="text-red-900">asddsaf</div>
+        <div class="description"></div>
+    </div>
+</div>
